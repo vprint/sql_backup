775be4a98af35c34f700c58f95f1c084bd18d6ea
vprint <valentin.printemps@gmail.com>
2023-11-06 23:19:04 +0100

    Amélioration du typage
    Amélioration des types et formatage

--- Function/get_bbox.sql
+++ Function/get_bbox.sql
@@ -1,14 +1,7 @@
-CREATE OR REPLACE FUNCTION carto.get_bbox(ids text)
- RETURNS TABLE(id text, bbox geometry)
- LANGUAGE plpgsql
- STABLE PARALLEL SAFE
-AS $function$
-BEGIN
-  RETURN QUERY
-    SELECT f.id::text, 
-    	ST_Envelope(f.geom) AS bbox
-    FROM carto.td_features f
-    WHERE f.id = ANY(string_to_array(ids, ','));
+CREATE OR REPLACE FUNCTION carto.get_bbox(ids text) RETURNS TABLE(id text, bbox geometry) LANGUAGE plpgsql STABLE PARALLEL SAFE AS $function$ begin RETURN QUERY
+SELECT f.id::text,
+  ST_Envelope(f.geom) AS bbox
+FROM carto.td_features f
+WHERE f.id = ANY(string_to_array(ids, ','));
 END;
-$function$
-;
+$function$;
--- Function/get_typology.sql
+++ Function/get_typology.sql
@@ -1,12 +1,13 @@
-CREATE OR REPLACE FUNCTION carto.get_typology()
- RETURNS TABLE(typology_name text, id_style text, id_typology text, geom_type text)
- LANGUAGE plpgsql
- STABLE STRICT
-AS $function$
-BEGIN
-   	RETURN QUERY
-   		SELECT t.typology_name::text, t.id_style::text, t.id_typology::text, t.geom_type::text   		
-   	FROM carto.td_typology t;
+CREATE OR REPLACE FUNCTION carto.get_typology() RETURNS TABLE(
+              typology_name text,
+              id_style double precision,
+              id_typology double precision,
+              geom_type text
+       ) LANGUAGE plpgsql STABLE STRICT AS $function$ BEGIN RETURN QUERY
+SELECT t.typology_name::text,
+       t.id_style::float,
+       t.id_typology::float,
+       t.geom_type::text
+FROM carto.td_typology t;
 END;
-$function$
-;
+$function$;
